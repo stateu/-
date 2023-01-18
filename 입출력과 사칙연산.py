eda29e811adc4c16611ba72b74d2add7e5534da7
stateu <stateu024@gmail.com>
2023-01-18 11:13:35 +0900

    0118 11:13

--- 입출력과 사칙연산.py
+++ 입출력과 사칙연산.py
@@ -1,46 +1,45 @@
-# # 3003 킹 퀸 룩 비숏 나이트 폰
-# def check_cnt(origin, target):
-#     plus = 0
-#     if origin == target:
-#         return plus
-#
-#     if origin > target:
-#         while True:
-#             plus += 1
-#             origin -= 1
-#
-#             if origin == target:
-#                 return -plus
-#                 # break
-#
-#     if origin < target:
-#         while True:
-#             plus += 1
-#             origin += 1
-#
-#             if origin == target:
-#                 return plus
-#                 # break
-#
-#
-# List = list(map(int, input().split(' ')))
-#
-# targetList = [1, 1, 2, 2, 2, 8]
-# for origin, target in zip(List, targetList):
-#     print(check_cnt(origin, target), end=' ')
+# 3003 킹 퀸 룩 비숏 나이트 폰
+def check_cnt(origin, target):
+    plus = 0
+    if origin == target:
+        return plus
 
-# # 2588 곱셈
-# a = int(input())
-# b = input()
-#
-# for i in b[::-1]:
-#     print(a*int(i))
-# print(a*int(b))
+    if origin > target:
+        while True:
+            plus += 1
+            origin -= 1
 
-# # 10172 개
-# print('''|\\_/|
-# |q p|   /}
-# ( 0 )"""\\
-# |"^"`    |
-# ||_/=\\\\__|''')
+            if origin == target:
+                return -plus
+                # break
 
+    if origin < target:
+        while True:
+            plus += 1
+            origin += 1
+
+            if origin == target:
+                return plus
+                # break
+
+
+List = list(map(int, input().split(' ')))
+
+targetList = [1, 1, 2, 2, 2, 8]
+for origin, target in zip(List, targetList):
+    print(check_cnt(origin, target), end=' ')
+
+# 2588 곱셈
+a = int(input())
+b = input()
+
+for i in b[::-1]:
+    print(a*int(i))
+print(a*int(b))
+
+# 10172 개
+print('''|\\_/|
+|q p|   /}
+( 0 )"""\\
+|"^"`    |
+||_/=\\\\__|''')
--- 조건문.py
+++ 조건문.py
@@ -1,61 +1,61 @@
-# # 2753 윤년
-# year = int(input())
-#
-# if year % 400 == 0:
-#     print(1)
-# elif (year % 4 == 0) and (year % 100 != 0):
-#     print(1)
-# else:
-#     print(0)
-#
-# # 2884 알람 시계
-# h, m = map(int, input().split(' '))
-#
-# if m >= 45:
-#     print(h, m - 45)
-# if m < 45:
-#     if h == 0:
-#         print(23, 60 - (45 - m))
-#     if h > 0:
-#         print(h - 1, 60 - (45 - m))
-#
-# # 2525 오븐 시계
-# h, m = map(int, input().split(' '))
-# need = int(input())
-# s = m + need
-#
-# if s // 60 >= 0:
-#     h += s // 60
-#     if h > 23:
-#         print(h - 24, s % 60)
-#     else:
-#         print(h, s % 60)
-# if s // 60 < 0:
-#     print(h, s % 60)
-#
-#
-# # 2480 주사위 세개
-# def prize(f, s, t):
-#     if f == s and s == t:
-#         print(10000 + f * 1000)
-#         return
-#
-#     if f == s and s != t:
-#         print(1000 + f * 100)
-#         return
-#
-#     if f == t and s != t:
-#         print(1000 + f * 100)
-#         return
-#
-#     if s == t and f != s:
-#         print(1000 + s * 100)
-#         return
-#
-#     if f != s and s != t:
-#         print(max(f, s, t) * 100)
-#         return
-#
-#
-# f, s, t = map(int, input().split(' '))
-# prize(f, s, t)
+# 2753 윤년
+year = int(input())
+
+if year % 400 == 0:
+    print(1)
+elif (year % 4 == 0) and (year % 100 != 0):
+    print(1)
+else:
+    print(0)
+
+# 2884 알람 시계
+h, m = map(int, input().split(' '))
+
+if m >= 45:
+    print(h, m - 45)
+if m < 45:
+    if h == 0:
+        print(23, 60 - (45 - m))
+    if h > 0:
+        print(h - 1, 60 - (45 - m))
+
+# 2525 오븐 시계
+h, m = map(int, input().split(' '))
+need = int(input())
+s = m + need
+
+if s // 60 >= 0:
+    h += s // 60
+    if h > 23:
+        print(h - 24, s % 60)
+    else:
+        print(h, s % 60)
+if s // 60 < 0:
+    print(h, s % 60)
+
+
+# 2480 주사위 세개
+def prize(f, s, t):
+    if f == s and s == t:
+        print(10000 + f * 1000)
+        return
+
+    if f == s and s != t:
+        print(1000 + f * 100)
+        return
+
+    if f == t and s != t:
+        print(1000 + f * 100)
+        return
+
+    if s == t and f != s:
+        print(1000 + s * 100)
+        return
+
+    if f != s and s != t:
+        print(max(f, s, t) * 100)
+        return
+
+
+f, s, t = map(int, input().split(' '))
+prize(f, s, t)
