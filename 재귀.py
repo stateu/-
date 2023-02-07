a78a95396e7bc4f81f9ae9ea8de95fd40289b16a
stateu <stateu024@gmail.com>
2023-02-07 17:27:01 +0900

    0207 1726

--- 재귀.py
+++ 재귀.py
@@ -1,51 +1,59 @@
-# # 10872 팩토리얼
-# def factorial(n):
-#     if n <= 1:
-#         return 1
-#     return n * factorial(n-1)
-#
-# N = int(input())
-# print(factorial(N))
-#
-# # 10870 피보나치 수5
-# def fibonacci(n):
-#     if n == 0:
-#         return 0
-#     if n == 1:
-#         return 1
-#     return fibonacci(n-1) + fibonacci(n-2)
-#
-# N = int(input())
-# print(fibonacci(N))
-#
-# # 25501 재귀의 귀재
-# def recursion(S, idx, e):
-#     global cnt
-#     cnt += 1
-#     if idx >= e:
-#         return 1
-#
-#     elif S[idx] != S[e]:
-#         return 0
-#
-#     else:
-#         return recursion(S, idx + 1, e - 1)
-#
-#
-# def is_palindrome(S):
-#     return recursion(S, 0, len(S) - 1)
-#
-#
-# T = int(input())
-#
-# for _ in range(T):
-#     cnt = 0
-#     temp = input()
-#     print(is_palindrome(temp), cnt)
+# 10872 팩토리얼
+def factorial(n):
+    if n <= 1:
+        return 1
+    return n * factorial(n-1)
 
-# # 11729 하노이 탑 이동 순서 star star
-# def move(L):
-#
-# N = int(input())
-#
-# cnt = 0
+N = int(input())
+print(factorial(N))
+
+# 10870 피보나치 수5
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
+N = int(input())
+print(fibonacci(N))
+
+# 25501 재귀의 귀재
+def recursion(S, idx, e):
+    global cnt
+    cnt += 1
+    if idx >= e:
+        return 1
+
+    elif S[idx] != S[e]:
+        return 0
+
+    else:
+        return recursion(S, idx + 1, e - 1)
+
+
+def is_palindrome(S):
+    return recursion(S, 0, len(S) - 1)
+
+
+T = int(input())
+
+for _ in range(T):
+    cnt = 0
+    temp = input()
+    print(is_palindrome(temp), cnt)
+
+# 11729 하노이 탑 이동 순서 star star
+def hanoi(n, start, end):
+    if n == 1:
+        print(start, end)
+        return
+
+    hanoi(n-1, start, 6-start-end) # n-1개의 원판을 start에서 임시 막대로
+    print(start, end) # 옮긴 방향 출력
+    hanoi(n-1, 6-start-end, end) # n-1개의 원판을 임시 막대에서 end로
+
+N = int(input())
+
+print(2**N-1)
+hanoi(N, 1, 3)
--- 집합과 맵.py
+++ 집합과 맵.py
@@ -1,32 +1,32 @@
-# # 10815 숫자카드
-# N = int(input())  # 상근이 개수
-# N_set = set(list(map(int,input().split())))
-#
-# M = int(input())
-# M_list = list(map(int, input().split()))
-#
-# # in 비교 : list << set / list의 경우 시간초과 났음.
-# for m in M_list:
-#     if m in N_set:
-#         print(1, end=' ')
-#     else:
-#         print(0, end=' ')
+# 10815 숫자카드
+N = int(input())  # 상근이 개수
+N_set = set(list(map(int,input().split())))
 
-# # 14425 문자열 집합
-# import sys
-#
-# N, M = map(int, sys.stdin.readline().split())
-# S_set = set()
-# for _ in range(N):
-#     S_set.add(sys.stdin.readline())
-#
-# cnt = 0
-# for _ in range(M):
-#     temp = sys.stdin.readline()
-#     if temp in S_set:
-#         cnt += 1
-#
-# print(cnt)
+M = int(input())
+M_list = list(map(int, input().split()))
+
+# in 비교 : list << set / list의 경우 시간초과 났음.
+for m in M_list:
+    if m in N_set:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
+
+# 14425 문자열 집합
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+S_set = set()
+for _ in range(N):
+    S_set.add(sys.stdin.readline())
+
+cnt = 0
+for _ in range(M):
+    temp = sys.stdin.readline()
+    if temp in S_set:
+        cnt += 1
+
+print(cnt)
 
 # 1620 나는야 포켓몬 마스터 이다솜
 import sys
@@ -35,12 +35,12 @@ pocket = dict()
 pocket_number = dict()
 
 for idx in range(N):
-    key = sys.stdin.readline()
+    key = sys.stdin.readline().strip()
     pocket[key] = idx
     pocket_number[str(idx)] = key
 
 for _ in range(M):
-    search = sys.stdin.readline()
+    search = sys.stdin.readline().rstrip()
     try:
         search = int(search)
     except:
@@ -48,5 +48,67 @@ for _ in range(M):
 
     if type(search) == str:
         print(pocket[search]+1)
-    else:
+    elif type(search) == int:
         print(pocket_number[str(search-1)])
+
+# 10816 숫자카드 2
+import sys
+from collections import defaultdict
+N = int(sys.stdin.readline())
+N_list = list(map(int, sys.stdin.readline().split()))
+N_dict = defaultdict(int)
+
+for idx in N_list:
+    N_dict[idx] += 1
+
+M = int(sys.stdin.readline())
+M_list = list(map(int, sys.stdin.readline().split()))
+
+for jdx in M_list:
+    print(N_dict[jdx], end=' ')
+
+# 1764 듣보잡
+import sys
+N,M = map(int, sys.stdin.readline().split())
+N_dict = dict()
+for _ in range(N):
+    temp = sys.stdin.readline().rstrip()
+
+    if temp in N_dict:
+        N_dict[temp] += 1
+    else:
+        N_dict[temp] = 1
+
+cnt = 0
+M_list = []
+for _ in range(M):
+    temp = sys.stdin.readline().rstrip()
+
+    if temp in N_dict:
+        cnt += 1
+        M_list.append(temp)
+
+print(cnt)
+for idx in sorted(M_list):
+    print(idx)
+
+# 1269 대칭 차집합
+import sys
+a, b = map(int, sys.stdin.readline().split())
+
+set_a = set(list(map(int, sys.stdin.readline().rstrip().split())))
+set_b = set(list(map(int, sys.stdin.readline().rstrip().split())))
+
+a_diff_b = set_a-set_b
+b_diff_a = set_b-set_a
+
+print(len(set_a^set_b)) # 대칭 차집합 연산 ^
+
+# 11478 서로 다른 부분 문자열의 개수
+temp = input().strip()
+sub_temp = []
+
+for idx in range(len(temp)):
+    for jdx in range(idx, len(temp)+1):
+        sub_temp.append(temp[idx:jdx])
+print(len(set(sub_temp))-1)
